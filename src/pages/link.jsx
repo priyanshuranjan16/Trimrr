1f396aaaadb0c730ee554b3251b1390c02b66fdc
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2025-06-20 17:49:52 +0530

    updated

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`http://localhost:5173/${link}`}
+            href={`https://trimrr-coral.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            http://localhost:5173/{link}
+            https://trimrr-coral.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`http://localhost:5173/${link}`)
+                navigator.clipboard.writeText(`https://trimrr-coral.vercel.app/${link}`)
               }
             >
               <Copy />
