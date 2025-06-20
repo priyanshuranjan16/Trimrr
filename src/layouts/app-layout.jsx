c63acfb523c9c7071eefe3fbc2e8c0e48ef62651
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2025-06-20 16:05:48 +0530

    implemented features

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,19 @@
+import Header from "@/components/header";
+import {Outlet} from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Fast and Reliable URL Shortener <br />
+        Cheers!!!
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
